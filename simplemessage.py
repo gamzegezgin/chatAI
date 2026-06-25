5516d7abef30b11006048151c5be668e084323f3
Gamze Gezgin <gamzegezgin@yahoo.com>
2026-06-26 01:46:08 +0300

    prompt

--- simplemessage.py
+++ simplemessage.py
@@ -1,20 +1,28 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.output_parsers import  StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", top_k=100, temperature=1)
-messages = [
-    SystemMessage(content="Translate following word in Turkish."),
-    HumanMessage(content="Sheeps")
-]
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1)
 
+#messages = [
+#    SystemMessage(content="Translate following word in Turkish."),
+#    HumanMessage(content="Sheep")
+#]
 
+system_prompt = "Translate following into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt), ("user", "{text}")
+    ]
+)
 parser = StrOutputParser()
 
-chain = model | parser
+chain = prompt_template | model | parser
+
 if __name__ == "__main__":
-#response = model.invoke(messages)
-    print(chain.invoke(messages))
+    print(chain.invoke({"language":"italian", "text":"hello mom"}))
