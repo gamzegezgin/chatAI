0d2747b7d1e60c432dcddf9e2926cde5c7aa56f1
Gamze Gezgin <gamzegezgin@yahoo.com>
2026-06-26 02:01:29 +0300

    langserve

--- simplemessage.py
+++ simplemessage.py
@@ -3,6 +3,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI
+from langserve import add_routes
 
 
 load_dotenv()
@@ -19,5 +21,16 @@ parser = StrOutputParser()
 
 chain = prompt_template | model | parser
 
+app = FastAPI(
+    title="Translator",
+    version="0.1",
+    description="translation chatbot"
+)
+
+add_routes(app,
+           chain,
+           path="/chain")
+
 if __name__ == "__main__":
-    print(chain.invoke({"language":"italian", "text":"hello mom"}))
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
