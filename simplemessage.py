9d63f7ea47026de09188618789e25a216c50b9fc
Gamze Gezgin <gamzegezgin@yahoo.com>
2026-06-25 22:24:50 +0300

    parser

--- simplemessage.py
+++ simplemessage.py
@@ -1,15 +1,20 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import  StrOutputParser
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", top_k=100, temperature=1)
 messages = [
-    SystemMessage(content="Write a story about following word in Turkish."),
+    SystemMessage(content="Translate following word in Turkish."),
     HumanMessage(content="Sheeps")
 ]
 
+
+parser = StrOutputParser()
+
+chain = model | parser
 if __name__ == "__main__":
-    response = model.invoke(messages)
-    print(response.content)
+#response = model.invoke(messages)
+    print(chain.invoke(messages))
